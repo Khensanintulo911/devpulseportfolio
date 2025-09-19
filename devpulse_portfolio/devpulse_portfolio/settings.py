6fbfb4b1a08c710e57023707fd61b6f0ce7d1ac8
Khensani <danielntulo@gmail.com>
2025-09-19 05:30:51 +0200

    Ready for Railway deployment

--- devpulse_portfolio/devpulse_portfolio/settings.py
+++ devpulse_portfolio/devpulse_portfolio/settings.py
@@ -123,10 +123,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / "static"]  # Optional: only if you have a /static folder
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
